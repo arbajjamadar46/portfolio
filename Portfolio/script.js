9833ddb91d5da52bcec3a56e153a2b6af810286e
ARBAJ JAMADAR <100834060+arbajjamadar46@users.noreply.github.com>
2023-12-30 01:42:43 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,6 @@
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+}
+
+// document.getElementById("toggleButton").addEventListener("click", toggleDarkMode);
